538a6a986a87dd63603234748034b2c978d5a1a2
Daniel Nakashima <daniel@objectlabs.com>
2017-05-18 15:07:46 -0700

    Add Create User form

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,9 +1,50 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {
+        password: '',
+        email: ''
+      }
+    }
+  }
+
+  createUser(user) {
+    console.log('creating ->', user)
+  }
+
   render() {
-    return (<h1>It works!</h1>)
+
+    const { user } = this.state
+
+    const handleChange = (e) => {
+      const { name, value } = e.target
+      user[name] = value
+      this.setState({ user: user })
+      console.log(this.state.user)
+    }
+
+    const handleSubmit = (e) => {
+      this.createUser(user)
+      e.preventDefault()
+    }
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <h3>Create User</h3>
+        <div class="form-group">
+          <label>email</label>
+          <input name= "email" type="text" onChange={handleChange}/>
+        </div>
+        <div class="form-group">
+          <label>password</label>
+          <input name= "password" type="text" onChange={handleChange}/>
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    )
   }
 }
